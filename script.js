fcc7337ab4d4862b9f828b44a804951bffdd06f0
streetlightkids <ltj_hq@hotmail.com>
2022-02-07 10:28:29 -0800

    first mobile version styling complete
    -need to work on footer share mode

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const button = document.getElementById("shareBtn");
+const footer = document.getElementById("footer");
 const tippyShareContent = document.getElementById("tippyShareContent");
 tippyShareContent.style.display = "flex";
 
-//while window width is >= 838 px use Tippy
-tippy(button, {
-  trigger: "click",
-  theme: "custom",
-  allowHTML: true,
-  interactive: true,
-  content: tippyShareContent,
-}); //end tippy()
+if (window.innerWidth >= 838) {
+  tippy(button, {
+    trigger: "click",
+    theme: "custom",
+    allowHTML: true,
+    interactive: true,
+    content: tippyShareContent,
+  }); //end tippy()
+} else {
+}
 
 //while window width is < 838 px change footer to share mode
