52f1168d87ac5fa9db0b9ba4d9407c2eefcd4dce
streetlightkids <ltj_hq@hotmail.com>
2022-02-07 10:48:22 -0800

    add and remove shareMode for mobile works

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const button = document.getElementById("shareBtn");
 const footer = document.getElementById("footer");
 const tippyShareContent = document.getElementById("tippyShareContent");
-tippyShareContent.style.display = "flex";
 
 if (window.innerWidth >= 838) {
+  tippyShareContent.style.display = "flex";
+
   tippy(button, {
     trigger: "click",
     theme: "custom",
@@ -12,6 +13,13 @@ if (window.innerWidth >= 838) {
     content: tippyShareContent,
   }); //end tippy()
 } else {
+  button.addEventListener("click", () => {
+    if (footer.classList.length == 1) {
+      footer.classList = "flex shareMode";
+    } else {
+      footer.classList = "flex";
+    }
+  });
 }
 
 //while window width is < 838 px change footer to share mode
