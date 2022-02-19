2fc8742b07858dc61d826962792682707d997c08
streetlightkids <ltj_hq@hotmail.com>
2022-02-19 11:59:11 -0800

    tippyShareContent display needs to be handled in JS
    Otherwise the mobile version doesn't appear correctly.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const footer = document.getElementById("footer");
 const tippyShareContent = document.getElementById("tippyShareContent");
 
 if (window.innerWidth >= 838) {
+  tippyShareContent.style.display = "flex";
   tippy(button, {
     trigger: "click",
     theme: "custom",
