43d3b38dca6fda9eeedcdb5d424b85591a5f2d6e
streetlightkids <ltj_hq@hotmail.com>
2022-02-18 17:18:49 -0800

    change tippyShareContent display to flex
    -delete styling code from js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const footer = document.getElementById("footer");
 const tippyShareContent = document.getElementById("tippyShareContent");
 
 if (window.innerWidth >= 838) {
-  tippyShareContent.style.display = "flex";
   tippy(button, {
     trigger: "click",
     theme: "custom",
